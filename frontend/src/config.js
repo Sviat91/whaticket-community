ae61567274d66fd358be9f26cbe1c295a5ef1276
Sviat91 <svyatoslav.upirov@gmail.com>
2026-06-07 11:45:35 +0200

    refactor: remove docker container check from getConfig function

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,4 @@
 function getConfig(name, defaultValue = null) {
-  // If inside a docker container, use window.ENV
-  if (window.ENV !== undefined) {
-    return window.ENV[name] || defaultValue;
-  }
-
   return import.meta.env[name] || defaultValue;
 }
 
